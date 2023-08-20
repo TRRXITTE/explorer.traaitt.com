36781f934623a8b562c04710c2285a04dd119855
traaitt | TRRXITTE Int., incorporate <traaitt@icloud.com>
2023-08-20 20:00:16 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,21 @@
+var api = 'https://main.trrxitte.com';
+var api_blockexplorer = 'https://main.trrxitte.com';
+var apiList = [
+	'https://main.trrxitte.com'
+];
+var blockTargetInterval = 24;
+var coinUnits = 800000000000000;
+var symbol = 'XTE';
+var refreshDelay = 100;
+// pools stats by MainCoins
+var poolsStat =
+	[
+		["traaittCASH offical","https://traaittsupply.cash:8117/stats"],
+		["Cryptonote.Club","http://tch.cryptonote.club:8117/stats"]
+    ];
+var nodesStat =
+	[
+		['http://us-west.traaitt.com:14486'],
+		['http://europe-west.traaitt.com:14486'],
+		['http://asia-southeast.traaitt.com:14486']
+    ];
